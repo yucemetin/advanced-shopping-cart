18ae0f0559649fd860ce84d7bc7d7733bc9f5a2b
yucemetin <red.banana988@gmail.com>
2022-09-16 20:35:46 +0300

    localstorage hook added

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 import ShoppingCart from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type ShoppingCartProviderProps = {
   children: ReactNode
@@ -28,10 +29,10 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
-  const [cartItems, setCartItems] = useState<CartItem[]>([])
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", [])
   const [isOpen, setIsOpen] = useState(false)
 
-  const cartQuantitiy = cartItems.reduce((quantity, item) => item.quantity + quantity, 0)
+  const cartQuantitiy = cartItems.reduce((quantity: number, item: any) => item.quantity + quantity, 0)
 
   const openCart = () => setIsOpen(true)
 

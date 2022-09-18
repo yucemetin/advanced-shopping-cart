586c88d4c45bfe94e071c20cfd8193a07bd69c0d
yucemetin <red.banana988@gmail.com>
2022-09-19 02:19:59 +0300

    social media icons fixed v2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
     return (
         <div>
-            <div className="icons" style={{position:"absolute",top:"50%",right:"35%"}}>
+            <div className="icons" style={{position:"absolute",top:"20%",right:"35%"}}>
                 <ul className="social-networks spin-icon">
                     <li><a href="https://linkedin.com/in/yucemetin" target="_blank" className="icon-linkedin">LinkedIn</a></li>
                     <li><a href="https://github.com/yucemetin" target="_blank" className="icon-github">GitHub</a></li>
